c32c28101c51f21bf03b7e7934f9f15b2d84fd69
Vivek Chandra <vchandrajoshi@twitter.com>
2020-08-30 10:00:16 +0530

    code chnges

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -133,27 +133,8 @@
       this_form.find(".sent-message").slideUp();
       this_form.find(".error-message").slideUp();
       this_form.find(".loading").slideDown();
-
-      if ($(this).data("recaptcha-site-key")) {
-        var recaptcha_site_key = $(this).data("recaptcha-site-key");
-        grecaptcha.ready(function () {
-          grecaptcha
-            .execute(recaptcha_site_key, {
-              action: "php_email_form_submit",
-            })
-            .then(function (token) {
-              php_email_form_submit(
-                this_form,
-                action,
-                this_form.serialize() + "&recaptcha-response=" + token
-              );
-            });
-        });
-      } else {
-        php_email_form_submit(this_form, action, this_form.serialize());
-      }
+      php_email_form_submit(this_form, action, this_form.serialize());
     }
-    recaptcha.reset();
     return true;
   });
 
